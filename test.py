4742027b5bb077cf20494c09ad46f5c75d769dff
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-01-30 16:24:54 +0100

    added test.py to visualise result on images for different crop 32x32, 80x80, original, mainly used for visualisation purposes and debug

--- test.py
+++ test.py
@@ -21,17 +21,32 @@ import numpy as np
 from train_utils import get_lr_function, get_loss_fun,get_optimizer,get_dataset_loaders,get_model,get_val_dataset
 from benchmark import compute_time_full, compute_time_no_loader,compute_loader_time
 from precise_bn import compute_precise_bn_stats
-from model import RegSeg
+from model import RegSeg, Enet_Regseg
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as F
+from torchinfo import summary
 
-path = "/home/j20morli/Documents/Projects/01_Schneider/data/models/model2"
+path = "/users/local/j20morli/Schneider2/third_party/RegSeg/checkpoints/0.01_hit_uav_enet_500epochs_latest"
+test_name = "1_130_60_0_07248.jpg"
+test_name = "0_80_30_0_06926.jpg"
+test_name = "1_60_80_0_00669.jpg"
+save_path = "/users/local/j20morli/Schneider2/examples/"
+image_path = "/users/local/j20morli/Schneider2/third_party//HIT-UAV/normal_json/train/" + test_name
+json_path = "/users/local/j20morli/Schneider2/third_party//HIT-UAV/normal_json/annotations/train.json"
 
+number = "12"
 dictio = torch.load(path, map_location=torch.device('cpu'))
 model_dic = dictio["model"]
 
-model = RegSeg(name="exp48_decoder26", num_classes=6)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+#model = RegSeg(name="exp48_decoder26", num_classes=2)
+model = Enet_Regseg(name="lowres", num_classes=2)
 model.load_state_dict(model_dic)
+model.to(device)
+
+model_test = Enet_Regseg(name="highres", num_classes=2)
+summary(model_test, input_size=(1, 3, 80, 80), depth=2)
 
 red = (255, 0, 0)
 blue = (0, 0, 255)
@@ -41,37 +56,11 @@ yellow = (255, 255, 0)
 other = (122, 122, 255)
 colors = [red, blue, green, violet, yellow, other]
 
-test_name = "1_110_30_0_02555.jpg"
-image = torchvision.io.read_image("/home/j20morli/Documents/Projects/01_Schneider/data/HIT-UAV/HIT-UAV/normal_json/test/" + test_name)
-save_path = "/home/j20morli/Documents/Projects/01_Schneider/data/examples/"
-number = "4"
-
-print(image.size())
-image = image.expand(3, -1, -1)
-small = image[:, 0:80, 210:290]
-plt.imshow(small.permute(1, 2, 0))
-plt.savefig(save_path + "image_" + number + "_10")
-plt.close()
-small = image[:, 0:32, 240:272]
-plt.imshow(small.permute(1, 2, 0))
-plt.savefig(save_path + "image_" + number + "_11")
-plt.close()
-image2 = image.unsqueeze(0)
-image2 = image2.float()
-print(image.size(), image.dtype)
-output = model(image2)
-print(output.size())
-output_image = torch.zeros_like(image)
-plt.imshow(image.permute(1, 2, 0))
-plt.savefig(save_path + "image_" + number + "_0")
-plt.close()
-
-image3 = torchvision.utils.draw_segmentation_masks(image, output.squeeze(0) > 0.5)
-image3 = image3.permute(1, 2, 0)
-plt.imshow(image3)
-plt.savefig(save_path + "image_" + number + "_1")
-plt.close()
-
+# utilities
+def print_and_save(image, save_name) :
+	plt.imshow(image)
+	plt.savefig(save_name)
+	plt.close()
 
 def get_boxes_ids(dicos, image_id) :
     boxes = []
@@ -87,52 +76,59 @@ def get_boxes_ids(dicos, image_id) :
             boxes.append([xmin, ymin, xmax, ymax])
     return torch.tensor(boxes), ids
 
+def add_bounding_boxes(image, json_content, save_name) :
+	json_annotations = json_content["annotations"]
+	min_id = json_content["images"][0]["id"]
+	max_id = json_content["images"][-1]["id"]
+	
+	for image_id in range(0, max_id + 1 - min_id) :
+		json_image = json_content["images"][image_id]
+		image_size =  (1, json_image["height"], json_image["width"])
+		image_filename = json_image["filename"]
+
+		if image_filename == test_name :
+			boxes, ids = get_boxes_ids(json_annotations, image_id + min_id)
+			colorsss = [colors[int(i)] for i in ids]
+			print(ids)
+			img = torchvision.utils.draw_bounding_boxes(image, boxes, fill=True, colors=colorsss)
+			img = F.to_pil_image(img)
+			print_and_save(img, save_name)
+			
+def pipeline(image, model, threshold, name) :
+	# show image before transformation
+	print_and_save(image.permute(1, 2, 0), save_path + "image_" + name + "_original" + ".png")
+	
+	output = model(image.unsqueeze(0).float().to(device)).cpu()
+	draw_mask = torchvision.utils.draw_segmentation_masks(image, output[0] > threshold)
+	print_and_save(draw_mask.permute(1, 2, 0), save_path + "image_" + name + "_mask" + ".png")
+
+	argmax = torch.argmax(output, dim=1)
+	print_and_save(argmax.permute(1, 2, 0), save_path + "image_" + name + "_argmax" + ".png")
+	
+	return output, draw_mask, argmax
+	
+	
+	
+# original image
+image = torchvision.io.read_image(image_path)
+image = image.expand(3, -1, -1) # to RGB standard
+print_and_save(image.permute(1, 2, 0), save_path + "image_" + number + "_original" + ".png")
+
+# smaller images
+small80 = image[:, 200:280, 300:380]
+small32 = image[:, 50:82, 210:242]
+print_and_save(small80.permute(1, 2, 0), save_path + "image_" + number + "_80" + ".png")
+print_and_save(small32.permute(1, 2, 0), save_path + "image_" + number + "_32" + ".png")
+
+threshold = 0
+voutput_original, draw_mask_original, argmax_original = pipeline(image, model, threshold, "original_" + number)
+pipeline(small80, model, threshold, "80_" + number)
+pipeline(small32, model, threshold, "32_"+ number)
 
 # show original bounding boxes
-json_path = "/home/j20morli/Documents/Projects/01_Schneider/data/HIT-UAV/HIT-UAV/normal_json/annotations/test.json"
 with open(json_path, "r+") as file :
     contents = file.read()
 
 json_content = json.loads(contents)
-json_annotations = json_content["annotation"]
-image_id = json_annotations[0]["image_id"]
-min_id = json_content["images"][0]["id"]
-max_id = json_content["images"][-1]["id"]
-print(len(json_content["images"]), max_id)
-
-for image_id in range(0, max_id + 1 - min_id) :
-    t1 = time.time()
-    #print("Time 0: ", time.strftime("%H:%M:%S", time.gmtime()))
-    imagee = json_content["images"][image_id]
-    image_size =  (1, imagee["height"], imagee["width"])
-    image_filename = imagee["filename"]
-
-    if image_filename == test_name :
-        boxes, ids = get_boxes_ids(json_annotations, image_id + min_id)
-        colorsss = [colors[int(i)] for i in ids]
-        img = torchvision.utils.draw_bounding_boxes(image, boxes, fill=True, colors=colorsss)
-        img = F.to_pil_image(img)
-        plt.imshow(img)
-        plt.savefig(save_path + "image_" + number + "_5")
-        plt.close()
+add_bounding_boxes(draw_mask_original, json_content,  save_path + "image_" + number + "mask_bounding_original" + ".png")
             
-    # mask = torch.zeros(image_size, dtype=torch.uint8)
-    # fig = plt.figure(frameon=False)
-    # fig.set_size_inches(float(image["width"]/100),float(image["height"]/100))
-    # ax = plt.Axes(fig, [0., 0., 1., 1.])
-    # ax.set_axis_off()
-    # fig.add_axes(ax)
-    # boxes, ids = get_boxes_ids(json_annotations, image_id + min_id)
-    # #print(boxes.size()) 
-    # t2 = time.time()
-    # #print("Time 1: ", time.strftime("%H:%M:%S", time.gmtime()))
-    # colors = self.get_colors(ids)
-    # mask = draw_bounding_boxes(mask, boxes, fill=True, colors=colors)
-    # mask = F.to_pil_image(mask)
-    # ax.imshow(mask, aspect='auto')
-    
-    # fig.savefig(self.targets_dir + split + "/" + image_filename)
-    # plt.close(fig)
-    # #plt.show()
-    # t3 = time.time()
-    # print(split[1:], "  Time : ", time.strftime("%H:%M:%S", time.gmtime()), " T1 :", t2 - t1, "T2 :", t3 - t2, "image_id : ", image_id)
