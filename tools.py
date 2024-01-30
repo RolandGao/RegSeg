cbe140f301b8796589f026e9eed125593a209c3c
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-01-30 15:58:39 +0100

    small fixes to gitignore and configs

---
+++ tools.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def get_mean_and_std(dataloader):
+    channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+    for data, _ in dataloader:
+        # Mean over batch, height and width, but not over the channels
+
+        channels_sum += torch.mean(data, dim=[0,2,3])
+        channels_squared_sum += torch.mean(data**2, dim=[0,2,3])
+        num_batches += 1
+
+    mean = channels_sum / num_batches
+
+    # std = sqrt(E[X^2] - (E[X])^2)
+    std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
+
+    return mean, std
